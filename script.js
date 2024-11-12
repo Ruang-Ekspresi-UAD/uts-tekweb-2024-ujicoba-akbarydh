547353ae045e7a7155cac276d8295d05e5f8955c
akbarydh <2300016085@webmail.uad.ac.id>
2024-11-12 22:53:42 +0700

    percobaan

---
+++ script.js
@@ -0,0 +1,11 @@
+<script>
+// Fungsi untuk mengarahkan ke Instagram 1
+function goToInstagram1() {
+    window.location.href = 'https://www.instagram.com/akbarydh_/profilecard/?igsh=MWgyN2Vyc2JvczVlcA=='
+}
+
+// Fungsi untuk mengarahkan ke Instagram 2
+function goToInstagram2() {
+    window.location.href = 'https://www.instagram.com/akbarydh_/profilecard/?igsh=MWgyN2Vyc2JvczVlcA=='
+}
+</script>
